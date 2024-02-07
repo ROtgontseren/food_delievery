0c6565a4c02aff1f768090c8f11a1df0c09fe1c5
unknown <otgoohghg@yahoo.com>
2024-02-07 14:13:40 +0800

    category modal

---
+++ food-admin/src/components/core/button/index.tsx
@@ -0,0 +1,38 @@
+import { Button as MuiButton, Stack } from "@mui/material";
+import { ReactNode } from "react";
+
+interface IButtonProps {
+  label: ReactNode;
+  disabled?: boolean;
+  btnType?: "contained" | "outlined" | "text";
+  onClick?: () => void;
+}
+
+export const Button = ({
+  label,
+  disabled = false,
+  btnType = "contained",
+  onClick,
+}: IButtonProps) => {
+  return (
+    <Stack>
+      <MuiButton
+        onClick={onClick}
+        color="success"
+        variant={btnType}
+        sx={{
+          p: 4,
+          fontSize: "1rem",
+          color:
+            btnType === "outlined" || btnType === "text" ? "#18ba51" : "white",
+          border: btnType === "outlined" ? 1 : 0,
+          borderColor: btnType === "outlined" ? "#18ba51" : "",
+        }}
+        disabled={disabled}
+        size="medium"
+      >
+        {label}
+      </MuiButton>
+    </Stack>
+  );
+};
--- food-server/src/router/uploadRouter.ts
+++ food-server/src/router/uploadRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import multer from "multer";
+import {upload} from "../utils/multer"
 import { uploadFile } from "../controller/uploadController";
 
 const router = Router();
-const upload = multer({dest: "./uploads"});
+// const upload = multer({dest: "./uploads"});
 
 router.route("/").post(upload.single("image"), uploadFile);
 
