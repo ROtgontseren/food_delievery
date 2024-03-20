fb065eb9f7a771415245554547d862f91da23244
unknown <otgoohghg@yahoo.com>
2024-03-20 18:37:53 +0800

    multer nemsen

--- food-server/src/middleware/multer.ts
+++ food-server/src/middleware/multer.ts
@@ -1,14 +1,14 @@
-import multer from "multer";
+// import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => {
-    cb(null, "/");
-  },
-  filename: (req, _file, cb) => {
-    cb(null, req.originalUrl);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (_req, _file, cb) => {
+//     cb(null, "/");
+//   },
+//   filename: (req, _file, cb) => {
+//     cb(null, req.originalUrl);
+//   },
+// });
 
-const upload = multer({ storage }).single("image");
+// const upload = multer({ storage }).single("image");
 
-export default upload;
+// export default upload;
